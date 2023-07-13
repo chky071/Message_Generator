9c0cd8453bd2fec5b86520c55c90d81959de5c4c
CMeeks <chad@meeksgrp.com>
2023-07-12 22:58:15 -0600

    Added messages

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-console.log('Hello World');
 const random = () =>{
     const rand_num=Math.ceil(Math.random()*5);
-    return rand_num;
-}
 
+    switch (rand_num) {
+        case 1:
+          return "Hi I can't come to the phone right now";
+          break;
+        case 2:
+          return "Be cool stay in school";
+          break;
+        case 3:
+          return "Don't do drugs";
+          case 4:
+          return "wax on wax off";
+          break;
+          case 5:
+          return "I'm running out of ideas";
+          break;
+          break;
+        default:
+          console.log('Selected fruit is unknown');
+      }
+}
 console.log(random());
